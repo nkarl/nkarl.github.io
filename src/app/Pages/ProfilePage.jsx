ffbc929e606acc8201f7e419fd4ea7dee1b5cda2
nkarl <21993921+nkarl@users.noreply.github.com>
2023-09-12 00:05:58 -0700

    Updated.

--- src/app/Pages/ProfilePage.jsx
+++ src/app/Pages/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import "./ProfilePage.css"
+import "./ProfilePage.css";
 
 function ProfilePage() {
   /*
@@ -8,21 +8,32 @@ function ProfilePage() {
   return (
     <>
       {/* header */}
-      <div className="header min-w-[100%] text-2xl grid place-items-center">
-        <div className="grid-cols-2">
-          <label className="border-gray-100 border-2 p-2" style={{ "border-radius": "4px 0 0 4px" }}>
+      <div className="header min-w-[100%] grid place-items-center">
+        <div className="grid-cols-2 border-2 rounded-md text-xl">
+          <label
+            className="border-gray-100 px-4"
+            style={{ "border-radius": "4px 0 0 4px" }}
+          >
             Search
           </label>
-          <input className="border-gray-100 p-2" style={{ "border-radius": "0 4px 4px 0" }}></input>
+          <input
+            className="border-gray-100"
+            style={{ "border-radius": "0 4px 4px 0" }}
+          ></input>
         </div>
       </div>
 
       {/* Body */}
       <div className="min-w-full bg-gray-400">
-        <div className="text-black w-screen bg-gray-300 p-2 grid grid-cols-12 gap-2">
-          <div className="min-h-screen bg-gray-200 col-start-3 col-end-11 grid grid-cols-12 grid-rows-6 gap-2 grid-flow-col">
-            <div className="bg-gray-100 col-start-2 col-span-3 row-start-1 row-span-2">Photo</div>
-            <div className="bg-gray-100 col-start-5 col-span-7 row-start-2 row-span-full">Body</div>
+        {/* TODO: set transform for different screen sizes */}
+        <div className="text-black w-screen bg-gray-300 p-2 grid grid-cols-12 gap-4">
+          <div className="min-h-screen bg-gray-200 col-start-3 col-end-11 grid grid-cols-12 grid-rows-6 gap-2 grid-flow-col auto-cols-max">
+            <div className="bg-gray-100 col-start-2 col-span-3 row-start-1 row-span-2 hover:scale-110 transform ease-in-out duration-200">
+              Photo
+            </div>
+            <div className="bg-gray-100 col-start-5 col-span-7 row-start-2 row-span-full hover:scale-[102%] transform ease-in-out duration-200">
+              Body
+            </div>
           </div>
         </div>
       </div>
@@ -32,7 +43,7 @@ function ProfilePage() {
         © 2023 Charles L. Nguyen. All Rights Reserved.
       </div>
     </>
-  )
+  );
 }
 
-export default ProfilePage
+export default ProfilePage;
