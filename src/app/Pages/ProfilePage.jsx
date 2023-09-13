cd2d94c2c15137ac81b5a9f8b1b92922571a6b5a
nkarl <21993921+nkarl@users.noreply.github.com>
2023-09-13 06:15:46 -0700

    Fixed component overflow on small screens.

--- src/app/Pages/ProfilePage.jsx
+++ src/app/Pages/ProfilePage.jsx
@@ -25,12 +25,12 @@ function ProfilePage() {
         </div>
 
         {/* Body */}
-        <div className="body bg-slate-400 grid place-items-stretch py-2 p-4">
+        <div className="body bg-slate-400 grid place-items-stretch py-2 p-4 box-content">
           <div className="min-h-[75vh] bg-slate-300 rounded-sm flex flex-col items-center content-center shadow-lg">
-            <div className="bg-[#ffb703] m-8 w-[48rem] h-[8rem] p-10 rounded-md text-black text-center text-4xl shadow-lg font-semibold mt-36 hover:scale-105 transform duration-300 ease-in-out hover:shadow-2xl hover:border-2 hover:border-slate-200">
+            <div className="bg-[#ffb703] m-8 w-[20rem] sm:w-[32rem] md:w-[48rem] h-[8rem] p-10 px-4 sm:p-10 rounded-md text-black text-center sm:text-2xl md:text-4xl shadow-lg font-semibold mt-36 hover:scale-105 transform duration-300 ease-in-out hover:shadow-2xl hover:border-2 hover:border-slate-200">
               ⛔ Site Under Construction. ⛔
             </div>
-            <div className="bg-slate-200 m-4 w-[32rem] h-[6rem] p-8 rounded-md text-black text-center text-xl shadow-lg hover:scale-105 transform duration-300 ease-in-out hover:shadow-xl hover:border-2 hover:border-slate-200">
+            <div className="bg-slate-200 m-4 sm:w-[20rem] md:w-[32rem] h-[6rem] p-8 rounded-md text-black text-center sm:text-md md:text-xl shadow-lg hover:scale-105 transform duration-300 ease-in-out hover:shadow-xl hover:border-2 hover:border-slate-200">
               Please come back another time.
             </div>
           </div>
