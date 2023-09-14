c3686b4d86cffdd55a67dca2257f1103a2a40ec5
nkarl <21993921+nkarl@users.noreply.github.com>
2023-09-14 09:03:50 -0700

    Updated.

--- src/app/Pages/ProfilePage.jsx
+++ src/app/Pages/ProfilePage.jsx
@@ -14,12 +14,15 @@ function ProfilePage() {
             <label
               className="border-gray-100 px-4"
               style={{ borderRadius: "4px 0 0 4px" }}
+              htmlFor="search"
             >
               Search
             </label>
             <input
-              className="border-gray-100"
+              className="border-gray-100 text-black px-2"
               style={{ borderRadius: "0 4px 4px 0" }}
+              type="text"
+              name="search"
             ></input>
           </div>
         </div>
@@ -27,7 +30,7 @@ function ProfilePage() {
         {/* Body */}
         <div className="body bg-slate-400 grid place-items-stretch py-2 p-4 box-content">
           <div className="min-h-[75vh] bg-slate-300 rounded-sm flex flex-col items-center content-center shadow-lg">
-            <div className="bg-[#ffb703] m-8 w-[20rem] sm:w-[32rem] md:w-[48rem] h-[8rem] p-10 px-4 sm:p-10 rounded-md text-black text-center sm:text-2xl md:text-4xl shadow-lg font-semibold mt-36 hover:scale-105 transform duration-300 ease-in-out hover:shadow-2xl hover:border-2 hover:border-slate-200">
+            <div className="bg-[#ffb703] m-8 w-[22rem] sm:w-[36rem] md:w-[42rem] h-[8rem] p-10 px-4 sm:p-10 rounded-md text-black text-center sm:text-2xl md:text-4xl shadow-lg font-semibold mt-36 hover:scale-105 transform duration-300 ease-in-out hover:shadow-2xl hover:border-2 hover:border-slate-200">
               ⛔ Site Under Construction. ⛔
             </div>
             <div className="bg-slate-200 m-4 sm:w-[20rem] md:w-[32rem] h-[6rem] p-8 rounded-md text-black text-center sm:text-md md:text-xl shadow-lg hover:scale-105 transform duration-300 ease-in-out hover:shadow-xl hover:border-2 hover:border-slate-200">
