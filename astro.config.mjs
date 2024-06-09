10dd7bf025afc7be62d71d5d447f3a0d9cd4dbe1
nkarl <21993921+nkarl@users.noreply.github.com>
2024-06-08 20:07:18 -0700

    Updated dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,33 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import remarkMermaid from "astro-diagram/remark-mermaid";
+//import remarkMermaid from "astro-diagram/remark-mermaid";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
+
+import rehypeMermaid from "rehype-mermaid";
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	markdown: {
+		syntaxHighlight: false, // disabled in order to use rehype plugins
 		shikiConfig: {
 			//theme: "material-theme",
 		},
-		remarkPlugins: [remarkMermaid],
+		remarkPlugins: [
+			remarkMath,
+			//remarkMermaid
+		],
+		rehypePlugins: [
+			rehypeKatex,
+			[rehypeMermaid, { strategy: "img-svg" }]
+		],
 	},
 	site: 'https://nkarl.github.io',
 	integrations: [
 		//starlight(),
-		mdx({
-			remarkPlugins: [
-				remarkMath,
-				remarkMermaid
-			],
-			rehypePlugins: [
-				rehypeKatex,
-				//rehypeMermaid
-			],
-		}),
+		mdx(),
 		sitemap(),
 	],
 });
